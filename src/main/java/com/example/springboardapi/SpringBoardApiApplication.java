304d054662a6cc32012ff7a009a82c3c1d1b1c0d
jykim <jykim86dev@gmail.com>
2022-05-09 23:06:21 +0900

    Initial commit

---
+++ src/main/java/com/example/springboardapi/SpringBoardApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoardApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoardApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboardapi/SpringBoardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoardApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
