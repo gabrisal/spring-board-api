ab5ab5563d95d160f1b119d979c3591dd4ec442b
jykim <jykim86dev@gmail.com>
2022-05-12 02:13:00 +0900

    feat: 프로젝트 패키지 구조 변경, 마이바티스 설정 추가

--- src/main/java/com/gabrisal/api/SpringBoardApiApplication.java
+++ src/main/java/com/gabrisal/api/SpringBoardApiApplication.java
@@ -1,8 +1,10 @@
-package com.example.springboardapi;
+package com.gabrisal.api;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan(basePackageClasses = SpringBoardApiApplication.class)
 @SpringBootApplication
 public class SpringBoardApiApplication {
 
