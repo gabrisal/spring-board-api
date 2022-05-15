acc53ca7458469af419e2f52f671feb5ce8ffdcd
jykim <jykim86dev@gmail.com>
2022-05-15 21:36:25 +0900

    feat: 태그 저장 로직 및 쿼리 추가

--- src/main/java/com/gabrisal/api/board/repository/BoardRepository.java
+++ src/main/java/com/gabrisal/api/board/repository/BoardRepository.java
@@ -1,6 +1,10 @@
 package com.gabrisal.api.board.repository;
 
+import com.gabrisal.api.board.dto.SearchBoardIn;
+import com.gabrisal.api.board.dto.SearchBoardOut;
 import com.gabrisal.api.board.model.Board;
+import com.gabrisal.api.board.model.BoardTag;
+import com.gabrisal.api.board.model.Tag;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -8,14 +12,18 @@ import java.util.List;
 @Mapper
 public interface BoardRepository {
 
-    Board selectBoardOne(int boardId);
+    SearchBoardOut selectBoardOne(SearchBoardIn in);
 
-    List<Board> selectBoardList();
+    List<SearchBoardOut> selectBoardList();
 
     int insertBoard(Board board);
 
     int updateBoard(Board board);
 
     int deleteBoardById(int boardId);
+
+    int insertTag(Tag tag);
+
+    int insertBoardTagRel(BoardTag boardTag);
 
 }
--- src/main/java/com/gabrisal/api/board/service/BoardService.java
+++ src/main/java/com/gabrisal/api/board/service/BoardService.java
@@ -1,9 +1,13 @@
 package com.gabrisal.api.board.service;
 
+import com.gabrisal.api.board.dto.AddBoardIn;
+import com.gabrisal.api.board.dto.SearchBoardIn;
+import com.gabrisal.api.board.dto.SearchBoardOut;
 import com.gabrisal.api.board.model.Board;
+import com.gabrisal.api.board.model.BoardTag;
+import com.gabrisal.api.board.model.Tag;
 import com.gabrisal.api.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,12 +21,12 @@ public class BoardService {
     private final BoardRepository repository;
 
     @Transactional(readOnly = true)
-    public Board getBoardById(int boardId) {
-        return repository.selectBoardOne(boardId);
+    public SearchBoardOut getBoardById(SearchBoardIn in) {
+        return repository.selectBoardOne(in);
     }
 
     @Transactional(readOnly = true)
-    public List<Board> getBoardList() {
+    public List<SearchBoardOut> getBoardList() {
         return repository.selectBoardList();
     }
 
@@ -36,5 +40,40 @@ public class BoardService {
 
     public int deleteBoardById(int boardId) {
         return repository.deleteBoardById(boardId);
+    }
+
+    public int insertTag(Tag tag) {
+        return repository.insertTag(tag);
+    }
+
+    public int insertBoardTagRel(BoardTag boardTag) {
+        return repository.insertBoardTagRel(boardTag);
+    }
+
+    public int saveBoard(AddBoardIn in) {
+        Board board = Board.builder()
+                    .boardTitle(in.getBoardTitle())
+                    .boardContent(in.getBoardContent())
+                    .frstRegUserId(in.getRegUserId())
+                    .lastUpdUserId(in.getRegUserId())
+                    .build();
+        int result = insertBoard(board);
+
+        for (Tag tag : in.getTagList()) {
+            // XXX: 태그에 시스템 컬럼이 필요할까?
+            tag.setFrstRegUserId(in.getRegUserId());
+            tag.setLastUpdUserId(in.getRegUserId());
+            insertTag(tag);
+
+            BoardTag boardTag = BoardTag.builder()
+                    .boardId(board.getBoardId())
+                    .tagId(tag.getTagId())
+                    .frstRegUserId(in.getRegUserId())
+                    .lastUpdUserId(in.getRegUserId())
+                    .build();
+            insertBoardTagRel(boardTag);
+        }
+
+        return result;
     }
 }
