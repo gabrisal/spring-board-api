583bc9f9c64a8965102895c725f93c70aade9065
jykim <jykim86dev@gmail.com>
2022-05-25 13:41:50 +0900

    fix: 메일송신자정보 세팅 추가

--- src/main/java/com/gabrisal/api/mail/service/MailService.java
+++ src/main/java/com/gabrisal/api/mail/service/MailService.java
@@ -3,6 +3,7 @@ package com.gabrisal.api.mail.service;
 import com.gabrisal.api.mail.vo.SendMailInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -14,11 +15,14 @@ import org.springframework.stereotype.Service;
 public class MailService {
 
     private final JavaMailSender mailSender;
+    @Value("${mail.sender}")
+    private String sender;
 
     public void sendMail(SendMailInfo sendMailInfo) {
         try {
             // 단순 텍스트 구성 메시지 생성
             SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+            simpleMailMessage.setFrom(sender);
             simpleMailMessage.setTo((String[]) sendMailInfo
                                                 .getReceiverMailList()
                                                 .toArray(new String[sendMailInfo.getReceiverMailList().size()]));
