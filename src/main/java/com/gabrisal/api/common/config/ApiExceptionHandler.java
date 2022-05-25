84d9b1698f7df60ee2d6d90865f9ff342f471264
jykim <jykim86dev@gmail.com>
2022-05-25 17:41:18 +0900

    feat: 경로 및 파일명 변경, 에러로깅메시지 변경

--- src/main/java/com/gabrisal/api/common/config/ApiExceptionHandler.java
+++ src/main/java/com/gabrisal/api/common/config/ApiExceptionHandler.java
@@ -1,5 +1,7 @@
-package com.gabrisal.api.common.exception;
+package com.gabrisal.api.common.config;
 
+import com.gabrisal.api.common.exception.ResponseMessage;
+import com.gabrisal.api.common.exception.StatusEnum;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,16 +9,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 @Slf4j
-public class ExceptionController {
+public class ApiExceptionHandler {
 
     /*
      TODO 다양한 에러처리 추가 필요
      */
     @ExceptionHandler({ Exception.class })
     public ResponseEntity<ResponseMessage> handleAll(final Exception ex) {
-        log.info("========== ERROR =======================================");
-        log.info("========== CLASS NAME : " + ex.getClass().getName());
-        log.error("error", ex);
+        log.error("[ERROR] Exception Type ::: " + ex.getClass().getSimpleName());
+        log.error("[ERROR] Message ::: ", ex.getMessage());
+        ex.printStackTrace();
         ResponseMessage resMsg = new ResponseMessage();
         resMsg.setErrCode(StatusEnum.SERVER_ERROR.getStatusCode());
         resMsg.setErrMsg(StatusEnum.SERVER_ERROR.getStatusValue());
