e51b9b65d22e6f8b04c7080f4302af8bcc34a494
jykim <jykim86dev@gmail.com>
2022-06-03 01:22:01 +0900

    feat: 유효성 메시지 추가, 하드코딩 메시지 수정 예정

--- src/main/java/com/gabrisal/api/common/config/ApiExceptionHandler.java
+++ src/main/java/com/gabrisal/api/common/config/ApiExceptionHandler.java
@@ -2,14 +2,32 @@ package com.gabrisal.api.common.config;
 
 import com.gabrisal.api.common.exception.ResponseMessage;
 import com.gabrisal.api.common.exception.StatusEnum;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
 
 @Slf4j
 @RestControllerAdvice
-public class ApiExceptionHandler {
+@RequiredArgsConstructor
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private final MessageSource messageSource;
 
     /*
      TODO 다양한 에러처리 추가 필요
@@ -25,5 +43,33 @@ public class ApiExceptionHandler {
         return ResponseEntity.badRequest().body(resMsg);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ResponseMessage resMsg = new ResponseMessage();
+        List<ObjectError> allErrors = ex.getBindingResult().getAllErrors();
+        for (ObjectError err : allErrors) {
+            log.error("[ERROR] ::: {}", err);
+            log.error("[ERROR] getCodes ::: {}", Arrays.toString(err.getCodes()));
+            String message = Arrays.stream(Objects.requireNonNull(err.getCodes()))
+                    .map(c -> {
+                        Object[] args = err.getArguments();
+                        Locale locale = LocaleContextHolder.getLocale();
+                        try {
+                            return messageSource.getMessage(c, args, locale);
+                        } catch (NoSuchMessageException e) {
+                            return null;
+                        }
+                    }).filter(Objects::nonNull)
+                    .findFirst()
+                    .orElse(err.getDefaultMessage());
+            log.error("[ERROR] message: {}", message);
+            // TODO: Valid 별 에러코드 분기
+            // XXX: getAllErrors 순서 확인
+            resMsg.setErrCode(StatusEnum.SERVER_ERROR.getStatusCode());
+            resMsg.setErrMsg(message);
+            return ResponseEntity.badRequest().body(resMsg);
+        }
+        return super.handleMethodArgumentNotValid(ex, headers, status, request);
+    }
 
 }
--- src/main/java/com/gabrisal/api/common/config/WebConfig.java
+++ src/main/java/com/gabrisal/api/common/config/WebConfig.java
@@ -1,14 +1,35 @@
 package com.gabrisal.api.common.config;
 
 import com.gabrisal.api.common.interceptor.PreLoggingInterceptor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new PreLoggingInterceptor());
     }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.addBasenames("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean getValidator(MessageSource messageSource) {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource);
+        return bean;
+    }
+
 }
