13f6536f6ac73a60e96834eb1be2a6abc01b21f1
jykim <jykim86dev@gmail.com>
2022-06-09 00:02:28 +0900

    feat: regUserId가 null인 경우 기본값 세팅 공백인 경우에는 현재 먹지않음ㅠ

--- src/main/java/com/gabrisal/api/board/dto/AddBoardIn.java
+++ src/main/java/com/gabrisal/api/board/dto/AddBoardIn.java
@@ -1,5 +1,7 @@
 package com.gabrisal.api.board.dto;
 
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.Nulls;
 import com.gabrisal.api.board.model.Tag;
 import com.gabrisal.api.common.validation.ValidationGroups;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -13,13 +15,12 @@ import javax.validation.constraints.Size;
 import java.util.List;
 
 @Schema(description = "게시판 등록/수정 요청")
-@Getter
-@Setter
+@Getter @Setter
 @NoArgsConstructor
 public class AddBoardIn {
 
     @Positive(groups = ValidationGroups.modifyBoardGroup.class)
-    @Schema(description = "게시글 ID", required = false, example = "1")
+    @Schema(description = "게시글 ID", example = "1")
     private int boardId;
 
     // XXX: 공통으로 모두 적용하고 특이케이스만 지정할 순 없을까?
@@ -33,13 +34,15 @@ public class AddBoardIn {
     @Schema(description = "게시글 내용", required = true, example = "내용")
     private String boardContent;
 
-    @NotBlank(groups = { ValidationGroups.createBoardGroup.class, ValidationGroups.modifyBoardGroup.class })
+    // XXX: 공백인 경우
+//    @NotBlank(groups = { ValidationGroups.createBoardGroup.class, ValidationGroups.modifyBoardGroup.class })
+    @JsonSetter(nulls = Nulls.SKIP)
     @Schema(description = "등록한 사용자 ID", required = true, example = "gabrisal")
-    private String regUserId;
+    private String regUserId = "JiYoung";
 
     @Size(max = 5
         , groups = { ValidationGroups.createBoardGroup.class, ValidationGroups.modifyBoardGroup.class })
-    @Schema(description = "태그 목록", required = false)
+    @Schema(description = "태그 목록")
     private List<Tag> tagList;
 
 }
