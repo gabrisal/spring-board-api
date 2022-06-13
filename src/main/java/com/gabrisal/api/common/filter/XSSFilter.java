cababa5de37f38d0a25f4be4ca3935fbdee91822
jykim <jykim86dev@gmail.com>
2022-06-13 10:08:36 +0900

    feat: XSSFilter multipart 예외 처리

--- src/main/java/com/gabrisal/api/common/filter/XSSFilter.java
+++ src/main/java/com/gabrisal/api/common/filter/XSSFilter.java
@@ -19,7 +19,13 @@ public class XSSFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         log.info("********* filter doFilter **********");
-        chain.doFilter(new XSSRequestWrapper((HttpServletRequest) request), response);
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        if (httpRequest.getMethod().equalsIgnoreCase("POST")
+                && httpRequest.getContentType().startsWith("multipart/form-data")) {
+           chain.doFilter(request, response);
+        } else {
+            chain.doFilter(new XSSRequestWrapper((HttpServletRequest) request), response);
+        }
     }
 
     @Override
